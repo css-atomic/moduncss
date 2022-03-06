8ce5d85446a96214ce8b45ddeb891050b7445d32
An <ancmsvn@gmail.com>
2022-03-07 02:38:58 +0700

    update snapshot

--- packages/postcss/test/unit/we-have-a-lot-of-class.spec.ts
+++ packages/postcss/test/unit/we-have-a-lot-of-class.spec.ts
@@ -2,5 +2,5 @@ import { expect, test } from 'vitest'
 import processCSS from '../integration/helper/process'
 
 test('a lot of classes', () => {
-  expect(processCSS(Array.from({ length: 1_000 }, (_, i) => `._${i} { width: ${i}; }`).join('\n'))).resolves.toMatchSnapshot('happy case')
+  expect(processCSS(Array.from({ length: 1_000 }, (_, i) => `._${i} { width: ${i}; }`).join('\n'))).resolves.toMatchSnapshot('default')
 })
